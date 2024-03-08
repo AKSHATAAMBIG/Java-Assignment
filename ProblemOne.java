f5f49ce7b2b557eabecf44621e4473b82b822695
AKSHATAAMBIG <90762445+AKSHATAAMBIG@users.noreply.github.com>
2024-03-08 18:57:41 +0530

    added the problem one code

---
+++ ProblemOne.java
@@ -0,0 +1,13 @@
+package assignment;
+
+//Write a java program to add two float numbers like 8.6 and 9.7
+
+public class ProblemOne {
+    public static float addFloatNumberStatic(float c, float d) {
+        return c + d;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(addFloatNumberStatic(8.6f, 9.7f));
+    }
+}
